14f60df235d8d80da50377f6a98ccee43cf926bd
ds-jhartmann <57985712+ds-jhartmann@users.noreply.github.com>
2025-05-15 10:41:54 +0200

    feat: fix tests

--- PoC/kafka-pull/kafka-broker-extension/src/test/java/org/eclipse/tractusx/edc/extensions/kafka/KafkaBrokerDataFlowControllerTest.java
+++ PoC/kafka-pull/kafka-broker-extension/src/test/java/org/eclipse/tractusx/edc/extensions/kafka/KafkaBrokerDataFlowControllerTest.java
@@ -19,47 +19,68 @@
  */
 package org.eclipse.tractusx.edc.extensions.kafka;
 
+import org.eclipse.edc.connector.controlplane.transfer.spi.flow.DataFlowPropertiesProvider;
+import org.eclipse.edc.connector.controlplane.transfer.spi.flow.TransferTypeParser;
 import org.eclipse.edc.connector.controlplane.transfer.spi.types.DataFlowResponse;
 import org.eclipse.edc.connector.controlplane.transfer.spi.types.TransferProcess;
+import org.eclipse.edc.connector.dataplane.selector.spi.DataPlaneSelectorService;
+import org.eclipse.edc.connector.dataplane.selector.spi.client.DataPlaneClient;
+import org.eclipse.edc.connector.dataplane.selector.spi.client.DataPlaneClientFactory;
+import org.eclipse.edc.connector.dataplane.selector.spi.instance.DataPlaneInstance;
 import org.eclipse.edc.junit.assertions.AbstractResultAssert;
 import org.eclipse.edc.policy.model.Policy;
+import org.eclipse.edc.spi.monitor.ConsoleMonitor;
 import org.eclipse.edc.spi.response.StatusResult;
+import org.eclipse.edc.spi.result.Result;
+import org.eclipse.edc.spi.result.ServiceResult;
 import org.eclipse.edc.spi.security.Vault;
 import org.eclipse.edc.spi.types.domain.DataAddress;
-import org.eclipse.tractusx.edc.extensions.kafka.auth.KafkaOAuthServiceImpl;
+import org.eclipse.edc.spi.types.domain.transfer.DataFlowResponseMessage;
+import org.eclipse.edc.spi.types.domain.transfer.DataFlowStartMessage;
+import org.eclipse.edc.spi.types.domain.transfer.FlowType;
+import org.eclipse.edc.spi.types.domain.transfer.TransferType;
+import org.eclipse.tractusx.edc.extensions.kafka.auth.KafkaOAuthService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.edc.spi.types.domain.edr.EndpointDataReference.*;
 import static org.eclipse.tractusx.edc.dataaddress.kafka.spi.KafkaBrokerDataAddressSchema.*;
 import static org.eclipse.tractusx.edc.extensions.kafka.KafkaBrokerDataFlowController.*;
 import static org.mockito.Mockito.*;
 
 class KafkaBrokerDataFlowControllerTest {
+    private final Vault vault = mock();
+    private final KafkaOAuthService oauthService = mock();
+    private final DataPlaneClient dataPlaneClient = mock();
+    private final DataPlaneClientFactory dataPlaneClientFactory = mock();
+    private final DataPlaneSelectorService selectorService = mock();
+    private final DataFlowPropertiesProvider propertiesProvider = mock();
+    private final TransferTypeParser transferTypeParser = mock();
     private KafkaBrokerDataFlowController controller;
-
     private DataAddress contentDataAddress;
-
     private TransferProcess transferProcess;
-
     private Policy policy;
 
-    private Vault vault;
-
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         contentDataAddress = DataAddress.Builder.newInstance()
                 .type(KAFKA_TYPE)
                 .property(TOPIC, "test-topic")
                 .property(BOOTSTRAP_SERVERS, "localhost:9092")
                 .property(PROTOCOL, "protocol")
                 .property(MECHANISM, "mechanism")
+                .property(GROUP_PREFIX, "test-group")
                 .property(POLL_DURATION, "PT5M")
                 .property(OAUTH_TOKEN_URL, "http://localhost:8080/token")
                 .property(OAUTH_REVOKE_URL, "http://keycloak:8080/revoke")
                 .property(OAUTH_CLIENT_ID, "client-id")
                 .property(OAUTH_CLIENT_SECRET_KEY, "clientSecretKey")
+                .property(TOKEN, "token")
                 .build();
 
         transferProcess = TransferProcess.Builder.newInstance()
@@ -69,11 +90,27 @@ class KafkaBrokerDataFlowControllerTest {
                 .correlationId("correlation")
                 .id("transferProcessId").build();
         policy = Policy.Builder.newInstance().assignee("test-group").build();
-        vault = mock();
 
-        KafkaOAuthServiceImpl oauthService = mock();
         when(oauthService.getAccessToken(any())).thenReturn("token");
-        controller = new KafkaBrokerDataFlowController(vault, oauthService);
+
+        when(transferTypeParser.parse(any())).thenReturn(Result.success(new TransferType("Kafka", FlowType.PULL)));
+
+        Map<String, Object> properties = contentDataAddress.getProperties();
+        when(propertiesProvider.propertiesFor(any(), any())).thenReturn(StatusResult.success(properties.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toString()))));
+
+        var dataPlaneInstance = DataPlaneInstance.Builder.newInstance().url("http://any").build();
+        when(selectorService.select(any(), anyString(), any())).thenReturn(ServiceResult.success(dataPlaneInstance));
+        when(selectorService.getAll()).thenReturn(ServiceResult.success(List.of(dataPlaneInstance)));
+
+        when(dataPlaneClientFactory.createClient(any())).thenReturn(dataPlaneClient);
+
+        var response = mock(DataFlowResponseMessage.class);
+        when(response.getDataAddress()).thenReturn(contentDataAddress);
+        when(dataPlaneClient.start(any(DataFlowStartMessage.class))).thenReturn(StatusResult.success(response));
+
+        controller = new KafkaBrokerDataFlowController(vault, oauthService, transferTypeParser, propertiesProvider,
+                selectorService, dataPlaneClientFactory, "random", new ConsoleMonitor(), () -> URI.create("http://localhost"));
     }
 
     @Test
@@ -106,10 +143,8 @@ class KafkaBrokerDataFlowControllerTest {
         assertThat(result.getContent()).isNotNull();
 
         AbstractResultAssert.assertThat(result).isSucceeded().extracting(DataFlowResponse::getDataAddress).satisfies(kafkaDataAddress -> {
-            assertThat(kafkaDataAddress.getType()).isEqualTo(EDR_SIMPLE_TYPE);
-            assertThat(kafkaDataAddress.getStringProperty(ID)).isEqualTo("transferProcessId");
-            assertThat(kafkaDataAddress.getStringProperty(ENDPOINT)).isEqualTo("localhost:9092");
-            assertThat(kafkaDataAddress.getStringProperty(CONTRACT_ID)).isEqualTo("contract");
+            assertThat(kafkaDataAddress.getType()).isEqualTo(KAFKA_TYPE);
+            assertThat(kafkaDataAddress.getStringProperty(BOOTSTRAP_SERVERS)).isEqualTo("localhost:9092");
             assertThat(kafkaDataAddress.getStringProperty(TOPIC)).isEqualTo("test-topic");
             assertThat(kafkaDataAddress.getStringProperty(GROUP_PREFIX)).isEqualTo("test-group");
             assertThat(kafkaDataAddress.getStringProperty(MECHANISM)).isEqualTo("mechanism");
