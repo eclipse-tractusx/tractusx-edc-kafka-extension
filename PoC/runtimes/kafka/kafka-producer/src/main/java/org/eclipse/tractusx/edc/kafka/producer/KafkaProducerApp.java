f323587ce3470fefcbc79670e6a9c4a1dbb03481
Alexander Wahl <alexander.wahl@doubleslash.de>
2025-05-16 13:01:54 +0200

    Adjust edc to accept multiple topics

--- PoC/runtimes/kafka/kafka-producer/src/main/java/org/eclipse/tractusx/edc/kafka/producer/KafkaProducerApp.java
+++ PoC/runtimes/kafka/kafka-producer/src/main/java/org/eclipse/tractusx/edc/kafka/producer/KafkaProducerApp.java
@@ -42,6 +42,7 @@ import static org.apache.kafka.common.config.SaslConfigs.*;
 public class KafkaProducerApp {
     static final String KAFKA_PRODUCTION_FORECAST_TOPIC = System.getenv().getOrDefault("KAFKA_PRODUCTION_FORECAST_TOPIC", "kafka-production-forecast-topic");
     static final String KAFKA_PRODUCTION_TRACKING_TOPIC = System.getenv().getOrDefault("KAFKA_PRODUCTION_TRACKING_TOPIC", "kafka-production-tracking-topic");
+    static final String KAFKA_STREAM_TOPIC = System.getenv().getOrDefault("KAFKA_STREAM_TOPIC", "kafka-stream-topic");
     static final String KEYCLOAK_CLIENT_ID = System.getenv().getOrDefault("KEYCLOAK_CLIENT_ID", "default");
     static final String KEYCLOAK_CLIENT_SECRET = System.getenv().getOrDefault("KEYCLOAK_CLIENT_SECRET", "mysecret");
     static final String VAULT_CLIENT_SECRET_KEY = System.getenv().getOrDefault("VAULT_CLIENT_SECRET_KEY", "secretKey");
@@ -49,6 +50,8 @@ public class KafkaProducerApp {
     static final String KEYCLOAK_REVOKE_URL = System.getenv().getOrDefault("KEYCLOAK_REVOKE_URL", "http://localhost:8080/realms/kafka/protocol/openid-connect/revoke");
     static final String KAFKA_BOOTSTRAP_SERVERS = System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092");
     static final String ASSET_ID = System.getenv().getOrDefault("ASSET_ID", "kafka-stream-asset");
+    static final String FORECAST_ASSET_ID = System.getenv().getOrDefault("FORECAST_ASSET_ID", "kafka-forecast-asset");
+    static final String TRACKING_ASSET_ID = System.getenv().getOrDefault("TRACKING_ASSET_ID", "kafka-tracking-asset");
     static final String EDC_API_AUTH_KEY = System.getenv().getOrDefault("EDC_API_AUTH_KEY", "password");
     static final String EDC_MANAGEMENT_URL = System.getenv().getOrDefault("EDC_MANAGEMENT_URL", "http://localhost:8081/management");
 
